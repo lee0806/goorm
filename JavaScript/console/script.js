0a95310b133d9becd62152373d7595451ab0e34a
sehyun lee <mh080411@kyonggi.ac.kr>
2025-12-23 16:40:42 +0900

    javascript : console

---
+++ JavaScript/console/script.js
@@ -0,0 +1,18 @@
+let greeting = '안녕하세여';
+
+console.log(greeting);
+console.log({a: 'a', b: 'b'})
+
+console.table({a: 'a', b: 'b'});
+
+console.error('에러')
+console.warn('경고')
+console.time('hello');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.timeEnd('hello');
+
+console.clear();
