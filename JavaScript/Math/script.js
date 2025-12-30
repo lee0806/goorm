dd8afd068eea8720d550bcbfdc733ce6b31b8c6f
sehyun lee <mh080411@kyonggi.ac.kr>
2025-12-30 11:33:35 +0900

    Math method

---
+++ JavaScript/Math/script.js
@@ -0,0 +1,26 @@
+const num1 = 10;
+const num2 = 20;
+
+let val = num1 + num2; // 덧셈
+val = num1 * num2; // 곱셈
+val = num1 - num2; // 뺄셈
+val = num1 / num2; // 나눗셈
+val = num1 % num2; // 나머지 연산
+
+// Math Object
+
+val = Math.E;
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.abs(-5);
+val = Math.min(2,4,4,6,6,7,8);
+val = Math.max(1,2,3,4,5,6,7);
+val = Math.random;
+
+// 1 ~ 10까지의 랜덤
+val = Math.floor((Math.random() * 10) + 1);
+
+console.log(val);
+
