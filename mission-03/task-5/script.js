7b502a782332ebc66ef5887bd87003b6d524da41
sehyun lee <mh080411@kyonggi.ac.kr>
2026-01-06 16:29:56 +0900

    mission 3 - task-5

---
+++ mission-03/task-5/script.js
@@ -0,0 +1,36 @@
+let a = 10;
+let b = 20;
+let c = 3
+
+console.log("덧셈")
+console.log(a + b);
+console.log("==================")
+
+console.log("뺄셈")
+console.log(b - a);
+console.log("==================")
+
+console.log("곱셈")
+console.log(a * b);
+console.log("==================")
+
+
+console.log("나눗셈")
+console.log(a / b);
+console.log("==================")
+
+
+console.log("나머지 연산")
+console.log(a % c);
+console.log("==================")
+
+console.log("제곱근")
+console.log(a ** 2);
+console.log("==================")
+
+console.log("PI")
+console.log(Math.PI);
+console.log("==================")
+
+console.log("cos")
+console.log(Math.cos(0));
