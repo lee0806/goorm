a4a0ad1579fe43e01064e1b22203a2ea296186b9
sehyun lee <mh080411@kyonggi.ac.kr>
2026-01-10 16:11:10 +0900

    map, filter, reduce

---
+++ JavaScript/map, filter, reduce/script.js
@@ -0,0 +1,31 @@
+// forEach
+// 값을 배열로 반환하는게 아닌, 특정 동작을 수행
+// const nums = [1, 2, 3];
+
+// nums.forEach(x => {
+//     console.log(x * 2);
+// });
+
+// map
+// 배열을 다른 배열로 변환해서 저장
+// 데이터를 가공할때 사용
+
+// const num = [1, 2, 3];
+
+// let num2 = num.map(x => x * 2);
+// console.log(num2);
+
+// filter
+// 조건에 부합한 결과만 남김
+
+// const list = [1, 23, 2, 3, 4, 5, 6, 10];
+
+// console.log(list.filter(x => x > 5));
+
+// reduce
+// 값을 누적해서 하나로 출력
+
+// let array = [1, 2, 3, 4, 5];
+
+// const result = array.reduce((x, y) => x + y);
+// console.log(result);
