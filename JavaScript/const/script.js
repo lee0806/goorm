7ddcefea445f779bb237c7f44eb7ecf25ab29581
sehyun lee <mh080411@kyonggi.ac.kr>
2026-01-10 12:48:06 +0900

    const

---
+++ JavaScript/const/script.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3, 4];
+
+const object = {name : '김똘똘', profession : 'A'};
+
+array.push(4);
+object.name = '김땅땅';
+
+console.log(array);
+console.log(object);
+
+array = [];
+object = {};
