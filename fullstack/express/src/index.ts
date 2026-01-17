1899088872885898539011691dd1cd89a3ca74f3
sehyun lee <mh080411@kyonggi.ac.kr>
2026-01-17 17:02:33 +0900

    코드 동작 확인

--- fullstack/express/src/index.ts
+++ fullstack/express/src/index.ts
@@ -2,3 +2,14 @@ import express from "express";
 import type { Request, Response } from "express";
 
 const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+app.get("/health", (req : Request, res : Response) => {
+    res.json({ OK : true, message : "server is running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`server listening on http://localhost:${PORT}`);
+});
