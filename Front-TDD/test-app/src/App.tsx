bc982b8a0580fe2d526aa174159b153c9ada0031
sehyun lee <mh080411@kyonggi.ac.kr>
2026-02-01 19:44:34 +0900

    change app file

--- Front-TDD/test-app/src/App.tsx
+++ Front-TDD/test-app/src/App.tsx
@@ -15,13 +15,7 @@ function App() {
         >
           {count}
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
